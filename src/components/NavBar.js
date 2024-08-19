ff655862c40f9a486d73734fbf26c79fd6011d8a
Eirik <eirik.imrik@gmail.com>
2024-08-19 17:28:11 +0200

    added logo to top left corner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,30 +6,43 @@ function NavBar() {
   return (
     <header className="header">
       <nav className="nav_container">
-        <ul className="nav__list">
-          <li>
-            <NavLink
-              to="/"
-              className="nav__link"
-              style={({ isActive }) => {
-                return isActive ? { color: "white" } : { textDecoration: "none" };
-              }}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              className="nav__link"
-              style={({ isActive }) => {
-                return isActive ? { color: "white" } : { textDecoration: "none" };
-              }}
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
+        <div className="logoDiv">
+          <h1>Ålesund Weather</h1>
+        </div>
+
+        <div className="listContainer">
+          <ul className="nav__list">
+            <li>
+              <NavLink
+                to="/"
+                className="nav__link"
+                style={({ isActive }) => {
+                  return isActive
+                    ? { color: "white" }
+                    : { textDecoration: "none" };
+                }}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className="nav__link"
+                style={({ isActive }) => {
+                  return isActive
+                    ? { color: "white" }
+                    : { textDecoration: "none" };
+                }}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="emptyDiv">
+          <h1>Ålesund Weather</h1>
+        </div>
       </nav>
     </header>
   );
