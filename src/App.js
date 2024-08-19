b00ab80f6b384bae3612f01795bd058c794c088f
Eirik <eirik.imrik@gmail.com>
2024-08-19 16:40:55 +0200

    added gradient and made components transparent

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<AboutUs/>}></Route>
       </Routes>
-      <Footer/>
+      <Footer/> 
     </Router>
   );
 }
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export const AboutUs = () => {
   return (
-    <div>
+    <div className="content">
       <h1>About Us</h1>
       <p>
         This is the first React app made by Eirik Imrik. It's supposed to be a
         simple weather app that shows the current wheater in the city you search
         for. Since it's the first application i make with React, it's not very
-        advanced. Please be kind.
+        advanced.
       </p>
     </div>
   );
