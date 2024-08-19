3989d88642ee67b14bf25e0df6d5ef06f9442a3d
Eirik <eirik.imrik@gmail.com>
2024-08-19 18:19:45 +0200

    updated about page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const AboutUs = () => {
   return (
     <div className="content">
-      <h1>About Us</h1>
+      <h1>About</h1>
       <p>
-        This is the first React app made by Eirik Imrik. It's supposed to be a
-        simple weather app that shows the current wheater in the city you search
-        for. Since it's the first application i make with React, it's not very
-        advanced.
+        Welcome to Ålesund Weather, your dedicated weather forecasting app for
+        Ålesund, Norway. Our mission is to provide residents and visitors with
+        the simple and accurate weather information for the city of Ålesund.{" "}
       </p>
+
+      <p>
+        This website is built and maintained by Eirik Imrik. For any inquiries,
+        please contact me at eirik.imrik@gmail.com.{" "}
+      </p>
+
+      <p>
+        If you want to support my work, feel free to buy me a <Link href="https://buymeacoffee.com/eirikimrik"> coffee</Link>
+        
+      </p>
+
+      <p>Thank you for visiting Ålesund Weather!</p>
     </div>
   );
 };
